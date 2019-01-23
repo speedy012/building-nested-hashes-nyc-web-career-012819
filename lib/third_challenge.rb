2eaa15f07842caf7f1ca4129310dc437c48acef7
Herman <hrivas5726@aol.com>
2019-01-23 15:34:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,6 +15,7 @@ def third_challenge
         #your key/value pairs here
         :name => "Romeo"
         :age => "15"
+        :status => "alive"
       },
       :hero_friends => []
    }, 
@@ -26,9 +27,14 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Capulet"
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
+        :name => "Juliet"
+        :age => "15"
+        :status => "alive"
         },
       :heroine_friends => []
    }
